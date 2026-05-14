93e3ac93b1cde279e2d728f225cf09d278605526
Thierno DIALLO <tdiallo@asi.fr>
2026-05-14 17:07:48 +0200

    Mise à jour branche develop

--- src/main/java/com/example/ecommerce/DT0/ProductRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.ecommerce.DT0;
-import lombok.Data;
-
-@Data
-public class ProductRequestDTO {
-    private String name;
-    private String description;
-    private Long categoryId;
-}
